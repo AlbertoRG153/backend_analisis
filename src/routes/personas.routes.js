6f53395f7b1e8817344d724616aea04bf109177f
GhostRaccoon30 <49474942+BryanCambar30@users.noreply.github.com>
2024-08-17 00:53:17 -0600

    'Login'

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -1,17 +1,8 @@
 // routes/personas.routes.js
 import { Router } from "express";
-import { saveEstudios, saveInfoFamilia, saveInfoLegal, saveInfoSanitaria, savePersona } from "../controllers/personas.controller";
 
 const router = Router();
 
-router.post('/persona/save', savePersona);
-
-router.post('/persona/familia/save', saveInfoFamilia);
-
-router.post('/persona/estudios/save', saveEstudios);
-
-router.post('/persona/legale/save', saveInfoLegal);
-
-router.post('/persona/sanitario/save', saveInfoSanitaria)
+router.
 
 export default router;
